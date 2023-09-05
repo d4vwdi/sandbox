bae4c1fd9acb48309387a014dcad531796ad3f4a
Dayvid Dalton <d4vwdi@gmail.com>
2023-09-05 11:07:32 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ConsultaCatalogo from "./components/ConsultaCatalogo";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Fatec ZL</h1>
+      <ConsultaCatalogo />
+    </div>
+  );
+}
